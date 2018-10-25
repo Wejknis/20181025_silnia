8653e7dc4592d2faf01c1de63e1f38e226d57cda
Wejknis <weja077@gmail.com>
2018-10-25 14:49:34 +0200

    licz silnie dodanie testu1

--- main.c
+++ main.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-    int licz_silnie(int podstawa) {
+long licz_silnie(int podstawa) {
     if (podstawa == 0) {
         return 1;
     } else {
-    int licznik;
-        int silnia=1;
+        long licznik;
+        long silnia = 1;
 
-        for (licznik = 1; licznik <= podstawa; licznik++7) {
-            silnia =silnia * licznik;
+        for (licznik = 1; licznik <= podstawa; ++licznik) {
+            silnia *= licznik;
         }
         return silnia;
     }
 }
 
+bool test_licz_silnie() {
+    if  (licz_silnie(0) == 1) {
+        printf("test_licz_silnie() działa:");
+        return true;
+    } else {
+        printf("test_licz_silnie nie działa!");
+        return false;
+    }
+}
+
+
+
 int main () {
-    int wynik;
-int podstawa;
+    long wynik;
+    long podstawa;
 
     printf("Podaj podstawę silni w przedziale od 0 do 15: \n");
-    scanf("%d", &podstawa);
+    scanf("%ld", &podstawa);
 
-        if (podstawa > 0 && podstawa <= 15) {
-            wynik = licz_silnie(podstawa);
-            printf("silnia: %d", wynik);
-        }
-            else{
-                printf("podałeś wartość spoza zakresu!");
-        }
+    if (podstawa > 0 && podstawa <= 15) {
+        wynik = licz_silnie(podstawa);
+        printf("silnia: %ld", wynik);
+    } else {
+        printf("podałeś wartość spoza zakresu!");
+    }
     return 0;
 }
